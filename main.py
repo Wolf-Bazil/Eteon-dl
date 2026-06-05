ceacd9a00346891bd93efa60681c2c627c2a45c3
Mon <88985912+Wolf-Bazil@users.noreply.github.com>
2026-06-05 14:53:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import yt_dlp
 url = input("playlist url here: ").strip()
 
 opts = {
-    'format': 'bestvideo+bestaudio/best',
-    'merge_output_format': 'mp4',
+    'format': 'bestaudio/best',
     "outtmpl": './downloads/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
     'noplaylist': False, 
 }
 
-print("Initializing Download..")
+print("Initializing Download...")
 
 with yt_dlp.YoutubeDL(opts) as ydl:
     ydl.download([url])
